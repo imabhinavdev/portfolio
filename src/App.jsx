4d177f7b05c9a75daea3da6eef6e22137b7f42e6
Abhinav Singh <abhinavas430@gmail.com>
2023-10-21 12:29:08 +0000

    Changes Homepage URL

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Abhinav Singh",
+  title: "A passionate computer science student",
+  email: "abhinavas430@gmail.com",
+  gitHub: "madabhi",
+  instagram: "alphaabhi.in",
+  linkedIn: "madabhi",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
